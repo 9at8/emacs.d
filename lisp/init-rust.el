1dd68838495e9604393bd3b5fa660d2a25f9d4cb
Aditya Thakral <aditya.1998thakral@gmail.com>
2019-01-07 13:50:25 -0500

    Add racer mode for rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -10,7 +10,8 @@
 
 (when (maybe-require-package 'flycheck-rust)
   (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+    (add-hook 'racer-mode-hook #'flycheck-mode)))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
